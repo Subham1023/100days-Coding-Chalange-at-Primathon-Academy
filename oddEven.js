00f7e97325e6e6eda49113d09d2c64ea217db81a
unknown <you@example.com>
2024-02-16 19:17:58 +0530

    file updated

--- oddEven.js
+++ oddEven.js
@@ -1,3 +1,13 @@
 
-
-let x = 20;
+let number = 20;
+oddEven();
+function oddEven(){
+    if (number % 5 == 0)
+  {
+    console.log("The number is odd");
+  }
+  else
+  {
+    console.log("The number is Even");
+}
+}
